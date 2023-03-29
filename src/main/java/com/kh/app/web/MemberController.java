8ab80ba4ebdd7db61f546e3dc1a48b87d9ee255b
jinhokim <llsoftll1@naver.com>
2023-03-29 13:27:28 +0900

    20230329

--- src/main/java/com/kh/app/web/MemberController.java
+++ src/main/java/com/kh/app/web/MemberController.java
@@ -1,13 +1,21 @@
 package com.kh.app.web;
 
+import com.kh.app.domain.entity.Member;
 import com.kh.app.domain.member.svc.MemberSVC;
 import com.kh.app.web.form.member.JoinForm;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -19,15 +27,39 @@ public class MemberController {
 
   //회원가입양식
   @GetMapping("/add")
-  public String joinForm(){
-
+  public String joinForm(Model model){
+    model.addAttribute("joinForm", new JoinForm());
     return  "member/joinForm";
   }
 
   //회원가입처리
   @PostMapping("/add")
-  public String join(JoinForm joinForm){
+  public String join(@Valid @ModelAttribute JoinForm joinForm, BindingResult bindingResult){
     log.info("joinForm={}", joinForm);
+    if(bindingResult.hasErrors()){
+      log.info("bindingResult={}", bindingResult);
+      return "member/joinForm";
+    }
+
+    //비밀번호 체크
+    if(!joinForm.getPasswd().equals(joinForm.getPasswdchk())) {
+      bindingResult.reject("passwd", "비밀번호가 일치하지 않습니다.");
+     log.info("bindingResult={}", bindingResult);
+      return "member/joinForm";
+    }
+
+    Member member = new Member();
+    member.setEmail(joinForm.getEmail());
+    member.setPasswd(joinForm.getPasswd());
+    member.setNickname(joinForm.getNickname());
+    member.setGender(joinForm.getGender());
+    member.setHobby(hobbyToString(joinForm.getHobby()));
+    member.setRegion(joinForm.getRegion());
+
+    memberSVC.save(member);
     return  "member/joinSuccess.html";
+  }
+  private String hobbyToString(List<String> hobby) {
+    return StringUtils.join(hobby, ",");
   }
 }
